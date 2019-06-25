4d1ec264b0e35df196c14e6f463c3886f9700d32
Fox <togfoxy@users.noreply.github.com>
2019-06-25 20:28:54 +1000

    Add files via upload

--- PersistentParking/PersistentParking.lua
+++ PersistentParking/PersistentParking.lua
@@ -4,6 +4,7 @@
 --** v0.01
 --** v0.02 - now writes to file only if on the ground and not flying
 --** v0.03 - better way of determining if plane is parked
+--** v0.04 - fixed a defect that gave really bad experiences if you crashed
 --********************************
 
 --********************************
@@ -178,7 +179,7 @@ function tfpp_main()
 	--get closest airport
 	tfpp_GetClosestAirport()	--thi sets the global variable strCurrentAirport
 	
-	if not bArrayInitialised then
+	if not bArrayInitialised and tfpp_bolOnTheGround == 1 and tfpp_InReplayMode == 0 then
 	
 		if bLoadPersistentParking then
 			tfpp_loadfromfile()
@@ -213,8 +214,9 @@ function tfpp_main()
 				
 			end
 
-			bArrayInitialised = true
+			
 		end
+		bArrayInitialised = true
 	end
 	
 	--get current geographic location
