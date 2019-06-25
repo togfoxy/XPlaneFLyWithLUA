5ead854c162f4601873c73fd8a78b10201b51ee6
Fox <togfoxy@users.noreply.github.com>
2019-06-25 20:52:07 +1000

    Add files via upload

--- CTAFAlerts/CTAF.lua
+++ CTAFAlerts/CTAF.lua
@@ -1,11 +1,67 @@
 --v0.1 initial release
+--v0.2 Now supports up to 11 AI planes
 
-dataref("bolInReplayMode","sim/time/is_in_replay")
+local fltAIPlaneLat = {0,0,0,0,0,0,0,0,0,0,0}
+local fltAIPlaneLong= {}
+
+dataref("tfCTAF_bolInReplayMode","sim/time/is_in_replay")
 dataref("bolOnTheGround", "sim/flightmodel/failures/onground_any")
 dataref("fltVerticalFPM","sim/flightmodel/position/vh_ind_fpm")
 
-dataref("fltPlane1Lat", "sim/multiplayer/position/plane2_lat")
-dataref("fltPlane1Long", "sim/multiplayer/position/plane2_lon")
+dataref("tmpLat1", "sim/multiplayer/position/plane1_lat")
+dataref("tmpLon1", "sim/multiplayer/position/plane1_lon")
+fltAIPlaneLat[1] = tmpLat1
+fltAIPlaneLong[1]= tmpLon1
+
+dataref("tmpLat2", "sim/multiplayer/position/plane2_lat")
+dataref("tmpLon2", "sim/multiplayer/position/plane2_lon")
+fltAIPlaneLat[2] = tmpLat2
+fltAIPlaneLong[2]= tmpLon2
+
+dataref("tmpLat3", "sim/multiplayer/position/plane3_lat")
+dataref("tmpLon3", "sim/multiplayer/position/plane3_lon")
+fltAIPlaneLat[3] = tmpLat3
+fltAIPlaneLong[3]= tmpLon3
+
+dataref("tmpLat4", "sim/multiplayer/position/plane4_lat")
+dataref("tmpLon4", "sim/multiplayer/position/plane4_lon")
+fltAIPlaneLat[4] = tmpLat4
+fltAIPlaneLong[4]= tmpLon4
+
+dataref("tmpLat5", "sim/multiplayer/position/plane5_lat")
+dataref("tmpLon5", "sim/multiplayer/position/plane5_lon")
+fltAIPlaneLat[5] = tmpLat5
+fltAIPlaneLong[5]= tmpLon5
+
+dataref("tmpLat6", "sim/multiplayer/position/plane6_lat")
+dataref("tmpLon6", "sim/multiplayer/position/plane6_lon")
+fltAIPlaneLat[6] = tmpLat6
+fltAIPlaneLong[6]= tmpLon6
+
+dataref("tmpLat7", "sim/multiplayer/position/plane7_lat")
+dataref("tmpLon7", "sim/multiplayer/position/plane7_lon")
+fltAIPlaneLat[7] = tmpLat7
+fltAIPlaneLong[7]= tmpLon7
+
+dataref("tmpLat8", "sim/multiplayer/position/plane8_lat")
+dataref("tmpLon8", "sim/multiplayer/position/plane8_lon")
+fltAIPlaneLat[8] = tmpLat8
+fltAIPlaneLong[8]= tmpLon8
+
+dataref("tmpLat9", "sim/multiplayer/position/plane9_lat")
+dataref("tmpLon9", "sim/multiplayer/position/plane9_lon")
+fltAIPlaneLat[9] = tmpLat9
+fltAIPlaneLong[9]= tmpLon9
+
+dataref("tmpLat10", "sim/multiplayer/position/plane10_lat")
+dataref("tmpLon10", "sim/multiplayer/position/plane10_lon")
+fltAIPlaneLat[10] = tmpLat10
+fltAIPlaneLong[10]= tmpLon10
+
+dataref("tmpLat11", "sim/multiplayer/position/plane11_lat")
+dataref("tmpLon11", "sim/multiplayer/position/plane11_lon")
+fltAIPlaneLat[11] = tmpLat11
+fltAIPlaneLong[11]= tmpLon11
 
 local loopcount = 0
 local fltRecentAirportLat
@@ -56,8 +112,9 @@ function tfCTAF_main()
 			--to see if they are at the same airport and within 2nm of that airport.
 
 	--check that the pilot is descending (i.e. on approach)
-	--print("check 1")
-	if fltVerticalFPM <= -100 then
+	--print("********************")
+	--print(fltAIPlaneLat[2])
+	if fltVerticalFPM <= -100 and tfCTAF_bolInReplayMode == 0 then
 		--print("check 2")
 		--ensure plane has not landed (still in flight)
 		if bolOnTheGround == 0 then
@@ -65,33 +122,23 @@ function tfCTAF_main()
 			--check for each aircraft
 			
 			strPilotsClosestAirport = tfCTAF_GetClosestAirport(LATITUDE, LONGITUDE)
-			strPlane1ClosestAirport = tfCTAF_GetClosestAirport(fltPlane1Lat,fltPlane1Long)
 			
-			--print(strPilotsClosestAirport .. strPlane1ClosestAirport)
-			
-			if strPilotsClosestAirport == strPlane1ClosestAirport then
-				
-				--print("check 4")
-				--see if AI is within 2nm of airport
-				fltPlane1DistanceToAirport = tfCTAF_distanceInNM(fltRecentAirportLat, fltRecentAirportLong, fltPlane1Lat,fltPlane1Long)
-				--print("****************")
-				--print(strRecentAirportName)
-				--print(fltRecentAirportLat)
-				--print(fltRecentAirportLong)
-				--print(fltPlane1Lat)
-				--print(fltPlane1Long)
-				
-				--print("distance = " .. fltPlane1DistanceToAirport)
-				--print("Pilot distance = " .. tfCTAF_distanceInNM(fltRecentAirportLat, fltRecentAirportLong, LATITUDE,LONGITUDE))
-				if fltPlane1DistanceToAirport <= 6 then	--remember this is in km
-					--AI is close enough for CTAF reports
-					if loopcount >= 3 then
-						--print("check 5")
-						XPLMSpeakString("CTAF alert")
-						loopcount = 0
-					else
-						--print("check 6: loop = " .. loopcount )
-						loopcount = loopcount + 1
+			for i=2,11 do
+				--print("I = " .. i )
+				--print(" and fltAIPlaneLat = " .. fltAIPlaneLat[4])
+				strAIPlaneClosestAirport = tfCTAF_GetClosestAirport(fltAIPlaneLat[i],fltAIPlaneLong[i])
+				if strPilotsClosestAirport == strAIPlaneClosestAirport then
+					--print("Check 3")
+					fltAIPlaneDistanceToAirport = tfCTAF_distanceInNM(fltRecentAirportLat, fltRecentAirportLong, fltAIPlaneLat[i],fltAIPlaneLong[i])
+					if fltAIPlaneDistanceToAirport <= 6 then	-- this is in km
+						--print ("Check 4")
+						if loopcount >= 3 then
+							XPLMSpeakString("CTAF alert")
+							loopcount = 0
+						else
+							loopcount = loopcount + 1
+							--print("loopcount = " .. loopcount)
+						end
 					end
 				end
 			end
