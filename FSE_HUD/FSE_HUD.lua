ac589223ae2d3c8a0de67192f6b4fc1960d32a3d
Fox <togfoxy@users.noreply.github.com>
2019-07-18 23:28:22 +1000

    v0.10

--- FSE_HUD/FSE_HUD.lua
+++ FSE_HUD/FSE_HUD.lua
@@ -19,14 +19,40 @@
 --		Added some automation - fixed the time/fuel used
 --v0.09
 --		Added voice alerts for VR peeps
+--v0.10
+--		Adjusted fuel calculations
+--		Stopped auto-register from registering a flight while sim is paused
+--		Added audible alarm if auto-end flight fails to work
 
 -- Requires tf_common_functions v0.02 or later
 
 local bolShowHotSpot = 1	--Change this to zero if you don't like the marker
 local intHudXStart = 15		--This can be changed
 local intHudYStart = 575	--This can be changed
--- ---------------------------------------------------------------------
 
+--These are user preferences VALUES. Do NOT change these
+local ACTION_NONE = 0			--no action
+local ACTION_ATCDEFAULT = 1			--ATC voice with default ATC text settings
+local ACTION_PAUSE = 4				--Pauses the sim
+local ACTION_AUTORESOLVE = 8	--auto-resolve
+
+--These are the preferences - set to one or more values described above
+local intActionTypeConnect = ACTION_AUTORESOLVE
+local intActionTypeRegisterFlight = ACTION_AUTORESOLVE
+local intActionTypeEndflight = ACTION_AUTORESOLVE
+
+------------------------------------------------------------------------
+-- Don't change anything below here
+------------------------------------------------------------------------
+require "graphics"
+require "tf_common_functions"local bolLoginAlertRaised = 0	--to ensure alerts aren't raised more than once
+
+local bolFlightRegisteredAlertRaised = 0
+local bolFlightNotEndedAlertRaised = 0
+local bolAttemptedToAutoConnect = 0
+local bolAttemptedToAutoRegister = 0
+
+local strHUDVersion = "v0.10"
 local intButtonHeight = 30			--the clickable 'panels' are called buttons
 local intButtonWidth = 140			--the clickable 'panels' are called buttons
 local intHeadingHeight = 20
@@ -42,36 +68,19 @@ local fltFuelUsedOnFlightKGs = 0	--what gets displayed on screen
 local bolCancelArmed = 0			--0 = cancel button is not armed. 1 = cancel button is armed
 local bolDeparted = 0				--0 = not yet departed from originating airport. Used for automation
 
-require "graphics"
-require "tf_common_functions"
-
---These values need to be exponential: 1,2,4,8 etc.
-local ACTION_NONE = 0			--no action
-local ACTION_ATCDEFAULT = 1			--ATC voice with default ATC text settings
-local ACTION_PAUSE = 4				--Pauses the sim
-local ACTION_AUTORESOLVE = 8	--auto-resolve
-
---These are user preferences
-local intActionTypeConnect = ACTION_AUTORESOLVE
-local intActionTypeRegisterFlight = ACTION_AUTORESOLVE
-local intActionTypeEndflight = ACTION_AUTORESOLVE
-
-local bolLoginAlertRaised = 0	--to ensure alerts aren't raised more than once
-local bolFlightRegisteredAlertRaised = 0
-local bolFlightNotEndedAlertRaised = 0
-local bolAttemptedToAutoConnect = 0
-local bolAttemptedToAutoRegister = 0
 
 local fltTimerForEndFlight = 10	--wait this many seconds before auto-ending a flight
 local fltBeginLandedTimer = 0	--the time at landing
 
-dataref("fltCurrentFuelWeightKGs" ,"sim/flightmodel/weight/m_fuel_total")
-dataref("fse_connected", "fse/status/connected")
-dataref("fse_flying", "fse/status/flying")
-dataref("bolOnTheGround", "sim/flightmodel/failures/onground_any")
-dataref("datGSpd", "sim/flightmodel/position/groundspeed")
-dataref("intATCTextOut", "sim/operation/prefs/text_out", "writeable")
-dataref("fltAltAGL", "sim/cockpit2/gauges/indicators/radio_altimeter_height_ft_pilot")
+dataref("tf_fltCurrentFuelWeightKGs" ,"sim/flightmodel/weight/m_fuel_total")
+dataref("tf_fse_connected", "fse/status/connected")
+dataref("tf_fse_flying", "fse/status/flying")
+dataref("tf_bolOnTheGround", "sim/flightmodel/failures/onground_any")
+dataref("tf_datGSpd", "sim/flightmodel/position/groundspeed")
+dataref("tf_intATCTextOut", "sim/operation/prefs/text_out", "writeable")
+dataref("tf_fltAltAGL", "sim/cockpit2/gauges/indicators/radio_altimeter_height_ft_pilot")
+dataref("tf_bolSimPaused", "sim/time/paused")
+dataref("tf_InReplayMode", "sim/time/is_in_replay")
 
 function tfFSE_DrawOutsidePanel()
 	--Draws the overall box
@@ -102,7 +111,7 @@ function tfFSE_DrawHeadingPanel()
 	local x2 = x1 + intButtonWidth + intButtonWidth
 	local y2 = y1 + intHeadingHeight
 	
-	graphics.draw_string((x1 + (intButtonWidth * 0.85)),(y1 + (intButtonHeight * 0.25)), "FSE HUD", "white")
+	graphics.draw_string((x1 + (intButtonWidth * 0.70)),(y1 + (intButtonHeight * 0.25)), "FSE HUD " .. strHUDVersion, "white")
 	
 	graphics.set_color(1, 1, 1, fltTransparency) --white
 	graphics.draw_rectangle(x1,y1,x2,y2)	
@@ -117,17 +126,18 @@ function tfFSE_DrawStatusPanel()
 	local y2 = y1 + intButtonHeight
 	local strTemp
 	
-	--print(fltFuelUsedOnFlightKGs .. ";" .. fltFuelStartWeightKGs ..";" ..fltCurrentFuelWeightKGs)
+	--print(fltFuelUsedOnFlightKGs .. ";" .. fltFuelStartWeightKGs ..";" ..tf_fltCurrentFuelWeightKGs)
 	--Work out what the panel values are
-	if fse_flying == 1 then
+	if tf_fse_flying == 1 then
 		fltFlightTimeSeconds = os.clock() - fltFlightStartTimeSeconds
 		
 		--This bit is a hack. The fuel is suppose to sync when you start flight but there is a timing problem so we do it here as well
-		if fltFlightTimeSeconds > 0 and fltFlightTimeSeconds < 5 then
-			fltFuelStartWeightKGs = fltCurrentFuelWeightKGs
+		if (fltFlightTimeSeconds > 0 and fltFlightTimeSeconds < 9) or(fltFuelUsedOnFlightKGs < 0) then
+			fltFuelStartWeightKGs = tf_fltCurrentFuelWeightKGs
 		end
 		
-		fltFuelUsedOnFlightKGs = tf_common_functions.round((fltFuelStartWeightKGs - fltCurrentFuelWeightKGs), 1)
+		fltFuelUsedOnFlightKGs = tf_common_functions.round((fltFuelStartWeightKGs - tf_fltCurrentFuelWeightKGs), 1)
+		
 		if fltFuelUsedOnFlightKGs < 0 then 
 			fltFuelUsedOnFlightKGs = 0 
 		end
@@ -136,7 +146,7 @@ function tfFSE_DrawStatusPanel()
 	strTemp = "Flight time: " ..  tf_common_functions.tf_SecondsToClockFormat(fltFlightTimeSeconds)
 	graphics.draw_string(x1 + (intButtonWidth * 0.05),y1 + (intButtonHeight * 0.6),strTemp,"white")
 	
-	local fltFuelUsedOnFlightGALs = tf_common_functions.round((fltFuelUsedOnFlightKGs * 0.2642),1)
+	local fltFuelUsedOnFlightGALs = tf_common_functions.round((fltFuelUsedOnFlightKGs / 2.60),1)
 	strTemp = "Fuel used: " .. fltFuelUsedOnFlightKGs .. " KGs / " .. fltFuelUsedOnFlightGALs .. " gals"
 	graphics.draw_string(x1 + (intButtonWidth * 0.05), y1 + (intButtonHeight * 0.2), strTemp, "white")	
 	
@@ -332,35 +342,35 @@ function tfFSE_DrawButtons()
 	--Examines FSE and flight state to draw the correct buttons in the correct place.
 	--There is a finite number of button states so are just hardcoded here
 	
-	if fse_connected == 0 then
-		--fse_connected == 0
+	if tf_fse_connected == 0 then
+		--tf_fse_connected == 0
 		tfFSE_DrawAlphaState()
 	else
-		if fse_flying == 0 then
-			--fse_connected == 1
-			--fse_flying == 0
+		if tf_fse_flying == 0 then
+			--tf_fse_connected == 1
+			--tf_fse_flying == 0
 			tfFSE_DrawBetaState()
 		else
 			if bolCancelArmed == 0 then
-				if bolOnTheGround == 1 and datGSpd <= 5 and os.clock() >= fltBeginLandedTimer + 4 then
-					--fse_connected == 1
-					--fse_flying == 1
+				if tf_bolOnTheGround == 1 and tf_datGSpd <= 5 and os.clock() >= fltBeginLandedTimer + 4 then
+					--tf_fse_connected == 1
+					--tf_fse_flying == 1
 					--bolCancelArmed == 0
-					--bolOnTheGround == 1
-					--datGSpd <= 5
+					--tf_bolOnTheGround == 1
+					--tf_datGSpd <= 5
 					--been stopped for at least 4 seconds
 					tfFSE_DrawEchoState()
 				else
-					--fse_connected == 1
-					--fse_flying == 1
+					--tf_fse_connected == 1
+					--tf_fse_flying == 1
 					--bolCancelArmed == 0
-					--bolOnTheGround == ?
-					--datGSpd == ?
+					--tf_bolOnTheGround == ?
+					--tf_datGSpd == ?
 					tfFSE_DrawCharlieState()
 				end
 			else
-				--fse_connected == 1
-				--fse_flying == 1
+				--tf_fse_connected == 1
+				--tf_fse_flying == 1
 				--bolCancelArmed == 1
 				tfFSE_DrawDeltaState()
 			end
@@ -400,13 +410,14 @@ function tfFSE_ConnectToServer()
 end
 
 function tfFSE_RegisterFlight()
-	if fse_flying == 0 and fse_connected == 1 then
+	if tf_fse_flying == 0 and tf_fse_connected == 1 and tf_bolSimPaused == 0 then
 		command_once("fse/flight/start")
 		bolCancelArmed = 0
-		fltFuelStartWeightKGs = fltCurrentFuelWeightKGs
+		fltFuelStartWeightKGs = tf_fltCurrentFuelWeightKGs
 		fltFlightStartTimeSeconds = os.clock()
 		bolFlightRegisteredAlertRaised = 0
 		bolAttemptedToAutoRegister = 0
+		bolFlightNotEndedAlertRaised = 0
 	end
 end	
 	
@@ -429,6 +440,7 @@ end
 function tfFSE_EndFlight()
 	command_once("fse/flight/finish")
 	bolAttemptedToAutoRegister = 0
+	bolFlightNotEndedAlertRaised = 0
 end	
 	
 function tfFSE_MouseClick()
@@ -442,17 +454,17 @@ function tfFSE_MouseClick()
 	local x2 = x1 + intButtonWidth + intButtonWidth
 	local y2 = y1 + intButtonHeight
 	
-	--print (fse_flying .. ";" .. bolCancelArmed .. ";" .. bolOnTheGround .. ";" .. datGSpd)
+	--print (tf_fse_flying .. ";" .. bolCancelArmed .. ";" .. tf_bolOnTheGround .. ";" .. tf_datGSpd)
 	
 	if MOUSE_X >= x1 and MOUSE_X <= x2 and MOUSE_Y >= y1 and MOUSE_Y < y2 then
-		if fse_connected == 0 then
+		if tf_fse_connected == 0 then
 			--Alpha state
 			--try to log in
 			tfFSE_ConnectToServer()
 			return
 		end
 		
-		if fse_connected == 1 and fse_flying == 0 then
+		if tf_fse_connected == 1 and tf_fse_flying == 0 then
 			--Beta state
 			tfFSE_RegisterFlight()
 			return
@@ -473,20 +485,20 @@ function tfFSE_MouseClick()
 				--print("alpha")
 		
 				--The left button has been clicked
-				if (fse_flying == 1 and bolCancelArmed == 0) and (bolOnTheGround == 0 or datGSpd > 5) then
+				if (tf_fse_flying == 1 and bolCancelArmed == 0) and (tf_bolOnTheGround == 0 or tf_datGSpd > 5) then
 					--This button is disabled. Do nothing
 					--print("beta")
 					return
 				end
 				
-				if fse_flying == 1 and bolCancelArmed == 1 then
+				if tf_fse_flying == 1 and bolCancelArmed == 1 then
 					--Really Cancel has been clicked
 					tfFSE_ReallyCancel()
 					--print("Charle")
 					return
 				end
 				
-				if fse_flying == 1 and bolCancelArmed == 0 and bolOnTheGround == 1 and datGSpd <= 5 and os.clock() >= fltBeginLandedTimer + 4 then
+				if tf_fse_flying == 1 and bolCancelArmed == 0 and tf_bolOnTheGround == 1 and tf_datGSpd <= 5 and os.clock() >= fltBeginLandedTimer + 4 then
 					--End flight has been clicked
 					--print("Trying to end flight")
 					tfFSE_EndFlight()
@@ -501,14 +513,14 @@ function tfFSE_MouseClick()
 		x2 = x1 + intButtonWidth
 		y2 = y1 + intButtonHeight			
 		if MOUSE_X >= x1 and MOUSE_X <= x2 and MOUSE_Y >= y1 and MOUSE_Y < y2 then
-			if fse_flying == 1 and bolCancelArmed == 0 then
+			if tf_fse_flying == 1 and bolCancelArmed == 0 then
 				--Charle state
 				--Cancel is armed
 				tfFSE_CancelArm()
 				return
 			end
 			
-			if fse_flying == 1 and bolCancelArmed == 1 then
+			if tf_fse_flying == 1 and bolCancelArmed == 1 then
 				--Deta state
 				--un-cancel flight
 				bolCancelArmed = 0
@@ -517,13 +529,16 @@ function tfFSE_MouseClick()
 		end
 	end
 end	
-	
 
 function tfFSE_Automation()	
 	--detect key events and react according to the action settings specified by the user
 	
+	if tf_InReplayMode == 1 or tf_bolSimPaused == 1 then
+		return
+	end
+	
 	--if taxiing and not logged in then check if there needs to be an action
-	if datGSpd > 5 and fse_connected == 0 and bolAttemptedToAutoConnect == 0 then
+	if tf_datGSpd > 5 and tf_fse_connected == 0 and bolAttemptedToAutoConnect == 0 then
 		--Check to see what action to do
 		if intActionTypeConnect >= ACTION_AUTORESOLVE then
 			tfFSE_ConnectToServer()
@@ -533,7 +548,7 @@ function tfFSE_Automation()
 	
 	--if taking off and not registered flight then check if there needs to be an action
 
-	if bolOnTheGround == 0 and fse_flying == 0 and fse_connected == 1 and bolAttemptedToAutoRegister == 0 then
+	if tf_bolOnTheGround == 0 and tf_fse_flying == 0 and tf_fse_connected == 1 and bolAttemptedToAutoRegister == 0 then
 		if intActionTypeRegisterFlight >= ACTION_AUTORESOLVE then
 			tfFSE_RegisterFlight()
 			bolAttemptedToAutoRegister = 1
@@ -542,8 +557,7 @@ function tfFSE_Automation()
 	
 	--if landed then check if there needs to be an action
 		
-
-	if bolOnTheGround == 1 and datGSpd < 5 and fse_flying == 1 and bolDeparted == 1 then
+	if tf_bolOnTheGround == 1 and tf_datGSpd < 5 and tf_fse_flying == 1 and bolDeparted == 1 then
 		if fltBeginLandedTimer == 0 then	--if timer is not started then start the timer.
 			fltBeginLandedTimer = os.clock()
 		end
@@ -557,11 +571,11 @@ function tfFSE_Automation()
 	end	
 
 	--capture if plane has taken off
-	if bolOnTheGround == 0 and bolDeparted == 0 then
+	if tf_bolOnTheGround == 0 and bolDeparted == 0 then
 		bolDeparted = 1
 	end
 	
-	if fse_flying == 0 then
+	if tf_fse_flying == 0 then
 		bolDeparted = 0
 		bolCancelArmed = 0
 		fltBeginLandedTimer = 0
@@ -572,17 +586,24 @@ function tfFSE_CheckAlarms()
 --Raise a voice alarm if auto-resolved didn't work
 
 	--check the auto-connect worked
-	if datGSpd > 15 and fse_connected == 0 and intActionTypeConnect >= ACTION_AUTORESOLVE and bolLoginAlertRaised == 0 then
+	if tf_datGSpd > 100 and tf_fse_connected == 0 and intActionTypeConnect >= ACTION_AUTORESOLVE and bolLoginAlertRaised == 0 then
 		--It seems the autoconnect didn't work. Need to say something.
 		XPLMSpeakString("Auto-connection failed")
 		bolLoginAlertRaised = 1
 	end
 	
 	--check that the auto-register worked
-	if bolOnTheGround == 0 and fse_flying == 0 and fse_connected == 1 and fltAltAGL > 300 and bolFlightRegisteredAlertRaised == 0 then
+	if tf_bolOnTheGround == 0 and tf_fse_flying == 0 and tf_fse_connected == 1 and tf_fltAltAGL > 300 and bolFlightRegisteredAlertRaised == 0 then
 		XPLMSpeakString("Flight auto-register failed")
 		bolFlightRegisteredAlertRaised = 1
 	end
+	
+	--check that the auto-end worked
+	if tf_bolOnTheGround == 1 and tf_fse_flying == 1 and tf_datGSpd < 5 and (os.clock() > (fltBeginLandedTimer + fltTimerForEndFlight + 30)) and bolFlightNotEndedAlertRaised == 0 and bolDeparted == 1 then
+		XPLMSpeakString("Flight auto-end failed")
+		bolFlightNotEndedAlertRaised = 1
+	end
+	
 end
 	
 do_every_draw("tfFSE_DrawThings()")
